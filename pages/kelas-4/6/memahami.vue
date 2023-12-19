0f42015a0505fc3d24804d05ec4f96ac63d38731
matthelosh <matthelosh@gmail.com>
2023-12-19 19:29:45 +0700

    Add Penjelasan Kelas-4 P6

--- pages/kelas-4/6/memahami.vue
+++ pages/kelas-4/6/memahami.vue
@@ -102,18 +102,42 @@
         <section class="infromasi mb-20 animate__animated animate__slideInLeft p-2" v-if="mode == 'informasi'">
             <h1 class="font-bold text-slate-700">Informasi Surah At-Tin</h1>
             <img src="/img/kelas-4/p6/info-attin.svg" alt="Informasi Surat At-Tin" />
+            
+        </section>
+        <section class="penjelasan mb-20 animate__animated animate__slideInLeft p-3" v-if="mode == 'penjelasan'">
+            <h1 class="font-bold text-slate-700">Memahami Pesan QS. At-Tin</h1>
+            <p class="my-2 text-justify">Apa arti at-Tin? Mengapa disebut surah at-Tin?</p>
+            <p class="my-2 text-justify">Surah at-Tin berada pada urutan nomor 95 dari 114 surat di dalam Al-Quran. Surah at-Tin terdiri dari 8 ayat. Surah at-Tin termasuk golongan surah Makkiyah karena diturunkan sebelum Nabi Muhammad saw berhijrah ke kota Madinah.</p>
+            <p class="my-2 text-justify">Nama surah ini diambil dari ayat yang pertama "wa ar-Tin" yan gberarti buah Tin.</p>
+            <p class="my-2 text-justify">Apa pesan pokok yang terkandung di dalam surah at-Tin? Mengapa surah ini diawali dengan sumpah atas nama buah tin dan zaitun? Mengapa Allah swt menjadikan manusia dalam bentuk sebaik-baiknya? Apa kelebihan manusia dibandingkan makhluk yang lain? Kenapa manusia yang sempurna itu bisa terjerumus ke tempat yan gserendah-rendahnya?</p>
+            <p class="my-2 text-justify">Mari kita simak penjelasannya di bawah ini.</p>
+            
+            <h3 class="font-bold text-sky-500">Ayat 1-3</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[0].text }}<span class="ayah">&#1633</span> 
+                    &nbsp;{{ ayahs[1].text }}<span class="ayah">&#1634</span>
+                    &nbsp;{{ ayahs[2].text }}<span class="ayah">&#1635</span>
+                </p>
+            </h3>
+            <p class="text-justify italic">(1) {{ artis[0] }}, (2) {{ artis[1] }}, (3) {{ artis[2] }}</p>
+            <p class="text-justify my-2">Buah Tin dan Zaitun banyak tumbuh di Syam (sekarang negara Palestina, Suriah, Yordania dan Lebanon) dan Baitul Maqdis (Yerusalem Palestina), tempatpara nabi diutus, antara lain Nabi Isa a.s. Gunung Sinai di Mesir adalah tempat Nabi Musa a.s. bermunajat. Sedangkan Makkah adalah tempat kelahiran dan pengutusan Nabi Muhammad saw. Ketiga nabi ini memiliki misi yang sama, yaitu mengajak manusia menuju tauhid atau mengesakan Allah.</p>
             <h1 class="mt-3">Perhatikan Ilustrasi di Bawah ini.</h1>
             <div class="flex flex-wrap justify-center gap-2">
                 <figure class="my-3">
-                    <img src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/02/15032226/Catat-Ini-Manfaat-Buah-Tin-yang-Sayang-untuk-Dilewatkan-01.jpg.webp" alt="Buah Tin" class="w-[200px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <img src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/02/15032226/Catat-Ini-Manfaat-Buah-Tin-yang-Sayang-untuk-Dilewatkan-01.jpg.webp" alt="Buah Tin" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
                     <figcaption class="text-sm italic text-gray-500 text-center">Buah Tin</figcaption>
                 </figure>
                 <figure class="my-3">
-                    <img src="https://yankes.kemkes.go.id/img/bg-img/gambarartikel_1656041283_496215.png" alt="Buah Zaitun" class="w-[200px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYBP-K0yk29URfM3DoFm9D_TSyJBzNnDmWsjq8DxtkmrzURmhXyX60jzl80QQIC1djejU&usqp=CAU" alt="Buah Zaitun" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
                     <figcaption class="text-sm italic text-gray-500 text-center">Buah Zaitun</figcaption>
                 </figure>
                 <figure class="my-3">
-                    <img src="https://static.republika.co.id/uploads/images/inpicture_slide/gunung-sinai-_120618112753-360.jpg" alt="Gunung Sinai" class="w-[200px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <img src="https://3.bp.blogspot.com/-h1JZCYaEUPo/VGdpfovuhAI/AAAAAAAAAjA/URo8Q_BeKek/s1600/b.m.jpg" alt="Baitul Maqdis" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <figcaption class="text-sm italic text-gray-500 text-center">Baitul Maqdis</figcaption>
+                </figure>
+                <figure class="my-3">
+                    <img src="https://static.republika.co.id/uploads/images/inpicture_slide/gunung-sinai-_120618112753-360.jpg" alt="Gunung Sinai" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
                     <figcaption class="text-sm italic text-gray-500 text-center">Gunung Sinai</figcaption>
                 </figure>
                 <figure class="my-3">
@@ -121,10 +145,64 @@
                     <figcaption class="text-sm italic text-gray-500 text-center">Kota Mekah</figcaption>
                 </figure>
             </div>
-        </section>
-        <section class="penjelasan mb-20 animate__animated animate__slideInLeft" v-if="mode == 'penjelasan'">
-            Penjelasan Ayat
-            <!-- <LazyQuizKelas4Lesson6Subtwo /> -->
+
+            <h3 class="font-bold text-sky-500">Ayat 4</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[3].text }}<span class="ayah">&#1636</span> 
+                </p>
+            </h3>
+            <p class="text-justify italic">(4) {{ artis[3] }}</p>
+            <p class="my-2 text-justify">Allah telah menciptakan manusia dalam bentuk yang sebaik-baiknya, jauh lebih sempurna daripada hewan. Allah juga membekali manusia dengan akal dan sifat-sifat yang unggul. Dengan kelebihan-kelebihan itulah Allah memberikan amanat kepada manusia sebagai pemimpin di bumi. Manusia sebagai pemimpin bertugas untuk melestarikan bumi.</p>
+
+            <h3 class="font-bold text-sky-500">Ayat 5</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[4].text }}<span class="ayah">&#1637</span> 
+                </p>
+            </h3>
+            <p class="text-justify italic">(5) {{ artis[4] }}</p>
+            <p class="my-2 text-justify">Bila manusia durhaka kepada Allah dan tidak menaati utusan-Nya, maka akan dikembalikan ke tempat yang serendah-rendahnya, yaitu ke neraka. Tubuh sempurna yang dilengkapi akal namun durhaka, tidak akan menyelamatkan manusia dari azab Allah.</p>
+
+            <h3 class="font-bold text-sky-500">Ayat 6</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[5].text }}<span class="ayah">&#1638</span> 
+                </p>
+            </h3>
+            <p class="text-justify italic">(6) {{ artis[5] }}</p>
+            <p class="my-2 text-justify">Orang-orang yang benar-benar beriman dan mengerjakan kebajikan akan dibalas dengan pahala yang tidak ada putus-putusnya dan diselamatkan dari neraka.</p>
+            <p class="my-2 text-justify">Coba kalian sebutkan, apa sajakah ciri-ciri orang beriman? Apa sajakah contoh kebajikan?</p>
+
+            <h3 class="font-bold text-sky-500">Ayat 7</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[6].text }}<span class="ayah">&#1639</span> 
+                </p>
+            </h3>
+            <p class="text-justify italic">(7) {{ artis[6] }}</p>
+            <p class="my-2 text-justify">Allah menciptakanmu dengan bentuk yang sempurna. Melewati berbagai tahap dari bayi, anak, kemudian akan menjadi remaja, dewasa, tua, hingga meninggal. Itu merupakan bukti yang paling jelas tentang kekuasaan Allah. Dia Mahakuasa untuk membangkitkanmu dari kematian. Maka, apa yang menyebabkan mereka mendustakanmu tentang hari pembalasan yaitu hari kiamat setelah adanya keterangan-keterangan yang gamblang itu?</p>
+            <div class="flex flex-wrap justify-center gap-2">
+                <figure class="my-3">
+                    <img src="https://storage.nu.or.id/storage/post/16_9/mid/8865_1696584334.webp" alt="Musim Kemarau" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <figcaption class="text-sm italic text-gray-500 text-center">Musim Kemarau</figcaption>
+                </figure>
+                <figure class="my-3">
+                    <img src="https://storage.nu.or.id/storage/post/16_9/mid/1578359127.jpg" alt="Musim Hujan" class="w-[150px] mx-auto rounded-full aspect-square object-cover border-4 border-slate-300 shadow-lg">
+                    <figcaption class="text-sm italic text-gray-500 text-center">Musim hujan</figcaption>
+                </figure>
+            </div>
+            <p class="my-2 text-justify">Apa yang terjadi jika musim kemarau? Ya, tanah terlihat retak. Daun berguguran. Tumbuhan kering. Terlihat seperti mati, bukan?</p>
+            <p class="my-2 text-justify">Apa yang terjadi jika musim hujan? Ya, di sekeliling kita terlihat hijau. Rumput dan pohon tumbuh. Padahal sebelumnya terlihat seperti mati. Begitulah perumpamaan hari kebangkitan. “Dan yang menurunkan air dari langit menurut ukuran (yang diperlukan) lalu dengan air itu Kami hidupkan negeri yang mati (tandus). Seperti itulah kamu akan dikeluarkan (dari kubur).” (Q.S. Az-Zukhruf/43:11)</p>
+            
+            <h3 class="font-bold text-sky-500">Ayat 8</h3>
+            <h3 class="quran">
+                <p class="text-rtl ">
+                    {{ ayahs[7].text }}<span class="ayah">&#1640</span> 
+                </p>
+            </h3>
+            <p class="text-justify italic">(8) {{ artis[7] }}</p>
+            <p class="my-2 text-justify">Jangan kaukira Allah menciptakan manusia secara sia-sia. Allah telah menurunkan aturan syariat. Dia akan memberi putusan dengan adil yaitu memberi pahala kepada orang yang taat dan menghukum orang yang bersalah.</p>
         </section>
     </div>
 
@@ -213,3 +291,29 @@ onMounted(() => {
     randomAyah()
 })
 </script>
+
+<style>
+.quran p {
+    font-size: 1.3em; font-weight: normal;
+    font-family: 'KFGQPC Uthman Taha Naskh', KFGQPC_Naskh, 'traditional arabic';
+    padding-left: 20px; padding-right: 20px;
+    direction: rtl; line-height: 34px;
+    font-weight: 600;
+}
+.ayah {
+  padding: 0 .3em;
+  display: inline-block;
+  position: relative;
+  font-size: .8em;
+}
+
+.ayah::after {
+  content: "\06DD";
+  display: block;
+  position: absolute;
+  font-size: 1.3em;
+  top: 50%;
+  left: 50%;
+  transform: translate( -50%, -50%);
+}
+</style>
